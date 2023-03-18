0a23f868b45125af4602f2d8bd32ab600c61dbe0
Ruby pal <rubyp20001@gmail.com>
2023-03-18 23:44:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let div = document.getElementById("output")
+  function getdata(){
+	  return new Promise((resolve,reject)=>{
+		  setTimeout(()=>{
+			  resolve("Hello, world!")
+		  },1000)
+	  })
+  }
+getdata.then((data)=>{
+	div.innerText = data;
+})
